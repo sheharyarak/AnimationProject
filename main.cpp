c1e6c079c0777040fef20eb36e3c1c2ea491250f
sheharyarak <31715063+sheharyarak@users.noreply.github.com>
2020-03-14 13:56:44 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "Bug.h"
 #include "alpha.h"
 #include <iostream>
+#include <chrono>
+#include <future>
+#include <vector>
 // #include <threads>
 
+static std::vector<std::future<void>>future_v;
+
 //NORMAL MAIN
-int		main()
+/* int		main()
 {
 	std::string word = "./Letters/";
 	std::cout << "Enter a word [PSA: Words don't have numbers]: ";
@@ -15,16 +20,19 @@ int		main()
 	Bug *barry = new Bug(0,50, a);
 	barry->set_p(p);
 	barry->set_v(5);
+	auto start = std::chrono::high_resolution_clock::now();
 	for(int i = 0; i < word.length(); i++)
 	{
 		barry->teleport(WIDTH*i,50);
 		draw(barry, word[i]);
 	}
+	auto end = std::chrono::high_resolution_clock::now();
 	std::cout << word <<".gif has been made." <<std::endl;
 	a->close();
+	std::chrono::duration<float> duration = end - start;
 	system("wsl animate result.gif");
 }
-/* 
+ */
 //THREADED MAIN
 int		main()
 {
@@ -37,15 +45,14 @@ int		main()
 	Bug *barry = new Bug(0,50, a);
 	barry->set_p(p);
 	barry->set_v(5);
-	std::thread LETTERS[word.length()];
 	for(int i = 0; i < word.length(); i++)
 	{
 		barry->teleport(WIDTH*i,50);
 		std::cout << "drawing "<<word[i]<<"."<<std::endl;
-		LETTERS[i] = std::thread(draw(barry, word[i]));
+		future_v.push_back(std::async(std::launch::async, draw, barry, word[i]));
 		std::cout << "finished drawing "<<word[i]<<"."<<std::endl;
 	}
 	std::cout << word <<".gif has been made." <<std::endl;
 	a->close();
 	system("wsl animate result.gif");
-} */
+}
